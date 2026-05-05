5e0ea8550344f3cb3e015394b1acd6508065e27d
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-05 17:14:32 -0400

    feature:ConexionDB

--- src/middlewares/validateToken.middleware.js
+++ src/middlewares/validateToken.middleware.js
@@ -2,7 +2,7 @@
 // x-user-id and x-user-role headers to this service.
 const validateToken = (req, res, next) => {
     const userId   = req.headers['x-user-id'];
-    const userRole = req.headers['x-user-role'];
+    const userRole = req.headers['x-user-rol'];
 
     if (!userId) {
         return res.status(401).json({ error: 'Token required' });
