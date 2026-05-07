6b316ae0f9589f948dc9d90133ad04308fb7823a
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-06 22:27:31 -0400

    a

--- src/middlewares/validateToken.middleware.js
+++ src/middlewares/validateToken.middleware.js
@@ -1,6 +1,7 @@
 // The API Gateway (Java) validates the JWT and forwards
 // x-user-id and x-user-role headers to this service.
 const validateToken = (req, res, next) => {
+    console.log('headers recibidos:', req.headers); 
     const userId   = req.headers['x-user-id'];
     const userRole = req.headers['x-user-role'];
 
