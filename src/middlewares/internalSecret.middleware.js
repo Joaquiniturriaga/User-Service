60c5b6145bcd80f8c717298b0c07a1d5507d3b64
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-26 17:42:00 -0400

    (feature):ID-2-USER-SERVICE:Implementacion de middleware de internal

---
+++ src/middlewares/internalSecret.middleware.js
@@ -0,0 +1,11 @@
+const validateInternalSecret = (req, res, next) =>{
+    const key = req.headers["x-internal-key"];
+
+    if (!key  || key !== process.env.INTERNAL_SECRET){
+        //Respuesta generica
+        return res-status(401).json({error: 'Unauthorized'});
+    }
+    next();
+}
+
+module.exports = {validateInternalSecret};
