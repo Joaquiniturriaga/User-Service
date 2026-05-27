b38d2b271e3fe127b8187ca8be80a89d2e460fe8
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-27 19:20:25 -0400

    (feat): ID-3-USER-SERVICE: Me olvide del implementar el middelware internal secreto

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const { router } = require('./src/routes/user.routes');
-
+const { validateInternalSecret } = require('./src/middlewares/internalSecret.middleware');
 const app = express();
 app.use(express.json());
 
-app.use('/api/users', router);
+app.use('/api/users', validateInternalSecret, router);
 
 app.get('/', (req, res) => {
     res.send('User service running');
