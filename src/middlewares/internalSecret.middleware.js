2ef87bf151e601419cc6fa72cc1e7614a8979960
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-27 22:07:35 -0400

    (chore):ID-3-USER-SERVICE:logs temporales

--- src/middlewares/internalSecret.middleware.js
+++ src/middlewares/internalSecret.middleware.js
@@ -1,5 +1,8 @@
 const validateInternalSecret = (req, res, next) =>{
     const key = req.headers["x-internal-key"];
+        console.log('>>> INTERNAL KEY recibida:', key);
+    console.log('>>> INTERNAL SECRET env:', process.env.INTERNAL_SECRET);
+
 
     if (!key  || key !== process.env.INTERNAL_SECRET){
         //Respuesta generica
